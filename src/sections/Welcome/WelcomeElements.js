f9bdb2a851fd7de3cc43235334882e6d9f18965e
John Martin <jrmartin182@gmail.com>
2021-01-06 16:00:30 -0700

    BUILD: created title text and styled

--- src/sections/Welcome/WelcomeElements.js
+++ src/sections/Welcome/WelcomeElements.js
@@ -8,13 +8,25 @@ export const WelcomeContainer = styled.section`
     height: 100%;
     display: flex;
     flex-direction: column;
-    justify-content: center;
+    justify-content: flex-end;
+`
+export const TextContainer = styled.div`
+    height: 275px;
+    width: 275px;
+    border: solid black 2px;
+    display: flex;
+    justify-content: flex-end;
     align-items: center;
+    flex-direction: column;
+    background-color: rgba(0,0,0,.3);
+    margin: 25px;
+
 `
 export const WelcomeTitleText = styled.h1`
     color: #FCFCFC;
-    font-size: 25px;
+    font-size: 60px;
     font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    padding: 10px;
 `
 export const SubTitleText = styled.p`
     color: #FCFCFC;
--- src/sections/Welcome/index.js
+++ src/sections/Welcome/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import {
-    WelcomeContainer, WelcomeTitleText, SubTitleText
+    WelcomeContainer, WelcomeTitleText, SubTitleText, TextContainer
 } from './WelcomeElements';
 
 const Welcome = () => {
     return (
         <WelcomeContainer>
-            <WelcomeTitleText>John Martin</WelcomeTitleText>
-            <SubTitleText>A fullstack web developer based out of Salt Lake City.</SubTitleText>
+            <TextContainer>
+                <WelcomeTitleText>Full Stack Web Developer</WelcomeTitleText>
+                {/* <SubTitleText>A fullstack web developer based out of Salt Lake City.</SubTitleText> */}
+            </TextContainer>
         </WelcomeContainer>
     )
 }
