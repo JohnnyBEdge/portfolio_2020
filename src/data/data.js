5314400adf3a664967aaa5ecd0e842c9217b0ecb
John Martin <jrmartin182@gmail.com>
2021-01-26 13:51:39 -0700

    CHORE: moved website screen shots to public folder

--- src/data/data.js
+++ src/data/data.js
@@ -4,42 +4,42 @@ export const projects = [
         desc: "A React built website for a local non-profit to spread awareness of their cause/mission. AWS Amplify to create, authenticate, and authorize admins to create and share events, a Formspree powered contact form and PayPal to collect donations.",
         github: "https://www.github.com",
         live: "https://www.utahhazaraassociation.org/",
-        img: require('../assets/images/projectImgs/uha.png'),
+        img: '/assets/images/uha.png',
     },
     {
         name: "Kieber-Emmons' Group Website",
         desc: "Updating and creating new content for a research group’s website. Using the client’s tech (Rapid Weaver), my focus was on UI/UX and content improvements. Involved interviewing group members to collect content, opinions on what was lacking/needed of the old site, then coming up with a scheme that was collectively agreed upon.",
         github: "https://www.github.com",
         live: "http://kieber-emmons.com/group/page/",
-        img: "tempImg2"
+        img: "/assets/images/mkgroup.png"
     },
     {
         name: "VN Bikers Website",
         desc: "A site made for the business I had while living in Vietnam. Using React with styled components and FormSpree, the goal was to concentrate on UI/UX and have better file structure.",
         github: "https://www.github.com",
         live: "https://vnbikers.netlify.app/",
-        img: "tempImg3"
+        img: "/assets/images/vnbikers.png"
     },
     {
         name: "1927 Solvay Website",
         desc: "A retro-styled website, inspired by Steve Krug’s book, Don’t Make Me Think, to showcase the scientists from the 1927 Solvay Conference. This was a fun side project to apply concepts from the book while exploring image mapping and further practice with React, JavaScript and Material UI. Because of the image mapping, this is best viewed through a desktop display.",
         github: "https://www.github.com",
         live: "https://1927solvay.netlify.app/",
-        img: "tempImg3"
+        img: "/assets/images/solvay.png"
     },
     {
         name: "Event Scheduler App",
         desc: "A school project where I created an event tracker to help remind you what's on your schedule. Simple, easy to use, easy to navigate, designed for mobile-use. Utilizes React, ExpressJS, Heroku, MongoDB and MomemntJS.",
         github: "https://www.github.com",
         live: "https://events-scheduler.herokuapp.com/",
-        img: "tempImg3"
+        img: "/assets/images/eventsapp.png"
     },
     {
         name: "My Wedding's Website",
         desc: "One of my earliest and most meaningful projects: the website I created for my wedding. Using only HTML, CSS and JavaScript, it contained all the wedding information, a photo gallery and with the use of Google Forms, a place to RSVP. Besides being a resource for the wedding, this site challenged my creative/artistic side. What I am most proud of is that I believe it captured both my wife's and my personalities.",
         github: "https://www.github.com",
         live: "https://thelefartins.com/",
-        img: "tempImg3"
+        img: "/assets/images/wedding.png"
     },
 
 ]
--- src/sections/Projects/index.js
+++ src/sections/Projects/index.js
@@ -7,6 +7,8 @@ import {
 } from './ProjectsElements';
 import { projects } from '../../data/data';
 import {FiChevronsLeft,FiChevronsRight} from 'react-icons/fi';
+import uhaImg from '../../assets/images/projectImgs/uha.png'
+import KEGroupImg from '../../assets/images/projectImgs/climb1 copy.jpg'
 
 const Projects = () => {
     const [current, setCurrent] = useState(0);
@@ -28,6 +30,7 @@ const Projects = () => {
 						<ProjTitle>{proj.name}</ProjTitle>
 					<TitleBottomBorder></TitleBottomBorder>
 					<ProjInfoContainer>
+						{/* <ProjImage src={require('../../assets/images/climb1.jpg')}/> */}
 						<ProjImage src={proj.img}/>
 						<ProjInfo>
 							<Info>{proj.desc}</Info>
@@ -44,19 +47,17 @@ const Projects = () => {
                 <ArrowContainer onClick={previousProj}>
                     <FiChevronsLeft size="3em"/>
                 </ArrowContainer>
-
-                
                     {projects.map((proj, idx) => {
                         return (
                             <div className={idx === current ? 'slide active' : 'slide'} key={idx}>
                                 {idx === current && (
                                     <Project id={idx} key={idx} current={current}>
-
                                         <TitleTopBorder></TitleTopBorder>
                                             <ProjTitle>{proj.name}</ProjTitle>
                                         <TitleBottomBorder></TitleBottomBorder>
         
                                         <ProjInfoContainer>
+                                            {/* <ProjImage src={'/assets/uha copy.png'}/> */}
                                             <ProjImage src={proj.img}/>
                                             <ProjInfo>
                                                 <Info>{proj.desc}</Info>
